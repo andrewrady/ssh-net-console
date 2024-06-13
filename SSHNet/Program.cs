399aecf25d575f288bb2a4ad9343f796e811c1cc
Andrew Rady <andrew.rady@ritterim.com>
2024-06-13 13:45:26 -0600

    init

---
+++ SSHNet/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Renci.SshNet;
+
+Console.WriteLine("SFTP Server Connection Test");
+const string host = "";
+const int port = 22;
+const string password = "";
+const string userName = "";
+using var client = new SftpClient(host, port, userName, password);
+try
+{
+   client.Connect();
+   Console.WriteLine(client.IsConnected);
+   Console.WriteLine(client.WorkingDirectory);
+}
+catch (Exception ex)
+{
+   Console.WriteLine(ex);
+}
